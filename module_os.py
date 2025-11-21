1dbb9896eefdd201e4fa8f4f4c1b185a17b5feb9
Hardcoder42 <amonbro92@gmail.com>
2025-11-21 21:07:36 +0700

    Add files via upload

--- module_os.py
+++ module_os.py
@@ -1,25 +1,25 @@
 import os
 
-#1 ЧАСТЬ
+# 1 ЧАСТЬ
 
-#создаем
-dir_name="Управление_файлами"
+# создаем
+dir_name = "Управление_файлами"
 try:
     os.mkdir(dir_name)
     print('Директория "Управление_файлами" успешно создана!')
 except Exception as e:
     print(f"Error: {e}")
 
-#переходим
+# переходим
 os.chdir(dir_name)
 print(f"Переходим в директорию: {os.getcwd()}")
 
-#1файл
+# 1файл
 with open("file1.txt", 'w', encoding='utf-8') as file1:
     file1.write('Первый файл\n')
 print('Файл "file1.txt" успешно создан!')
 
-#2файл
+# 2файл
 with open('file2.txt', 'w', encoding='utf-8') as file2:
     file2.write('Второй файл\n')
 print('Файл "file2.txt" успешно создан!')
@@ -29,16 +29,16 @@ files = os.listdir()
 for file in files:
     print(f"- {file}")
 
-#2 ЧАСТЬ
+# 2 ЧАСТЬ
 
-#удаляем 1 из созданных файлов
+# удаляем 1 из созданных файлов
 try:
     os.remove("file2.txt")
     print("Файл 'file2.txt' успешно уничтожен!")
 except Exception as e:
     print(f"Ошибка: {e}")
 
-#создаем поддиректорию
+# создаем поддиректорию
 dir_name2 = "Поддиректория"
 try:
     os.mkdir(dir_name2)
@@ -46,31 +46,31 @@ try:
 except Exception as e:
     print(f"Ошибка: {e}")
 
-#перемещение файла
+# перемещение файла
 try:
     os.rename("file1.txt", os.path.join(dir_name2, "file1.txt"))
     print('Файл "file1.txt"успешно перемещен...')
 except Exception as e:
     print(f"Ошибка: {e}")
 
-#Возврат в родительскую директорию для возможности удаления
+# Возврат в родительскую директорию для возможности удаления
 os.chdir("..")
 
-#удаление директории вместе с содержимым
+# удаление директории вместе с содержимым
 try:
     for item in os.listdir(dir_name):
         path = os.path.join(dir_name, item)
 
-        #на предмет файлов
+        # на предмет файлов
         if os.path.isfile(path):
             os.remove(path)
             print(f"Удален файл: {item}")
 
         # на предмет поддиректорий
         elif os.path.isdir(path):
-            #сначала очистка
+            # сначала очистка
             for sub_item in os.listdir(path):
-                sub_path=os.path.join(path, sub_item)
+                sub_path = os.path.join(path, sub_item)
                 if os.path.isfile(sub_path):
                     os.remove(sub_path)
             os.rmdir(path)
@@ -80,7 +80,7 @@ try:
 except Exception as e:
     print(f"Ошибочка вышла: {e}")
 
-#проверка содержимого
+# проверка содержимого
 print('\nОставшееся содержимое директории: ')
 files = os.listdir()
 for file in files:
